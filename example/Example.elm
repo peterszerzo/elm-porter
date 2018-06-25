9cc95636b1e1e83f5ec7dcc0b95fa358c2941fde
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-06-25 19:41:00 +0200

    elm-formatting the Example.elm file

--- example/Example.elm
+++ example/Example.elm
@@ -23,6 +23,7 @@ porterConfig =
     -- Porter works with a single Request and Response data types. They can both be anything, as long as you supply decoders :)
     , encodeRequest = Encode.string
     , decodeResponse = Decode.string
+
     -- Porter uses a message added to your Msg type for its internal communications (See `type Msg` below)
     , porterMsg = PorterMsg
     }
@@ -45,18 +46,16 @@ init =
       , response = ""
       , advanced_response = ""
       }
-    ,
-        Cmd.batch
-            [
-              -- Send a request through porter, specifying the response handler directly
-              Porter.send porterConfig Receive "Reverse me!"
+    , Cmd.batch
+        [ -- Send a request through porter, specifying the response handler directly
+          Porter.send porterConfig Receive "Reverse me!"
 
-              -- Or send multiple requests one after the other:
-              , Porter.request ("Reverse me too!")
-                |> Porter.andThen (\reversed_str -> Porter.request (reversed_str ++ " The Quick Brown Fox!"))
-                |> Porter.andThen (\reversed_str -> Porter.request (reversed_str ++ " A man a plan a canal: panama"))
-                |> Porter.sendRequest porterConfig Receive2
-            ]
+        -- Or send multiple requests one after the other:
+        , Porter.request ("Reverse me too!")
+            |> Porter.andThen (\reversed_str -> Porter.request (reversed_str ++ " The Quick Brown Fox!"))
+            |> Porter.andThen (\reversed_str -> Porter.request (reversed_str ++ " A man a plan a canal: panama"))
+            |> Porter.sendRequest porterConfig Receive2
+        ]
     )
 
 
@@ -86,6 +85,7 @@ update msg model =
 
         Receive response ->
             ( { model | response = response }, Cmd.none )
+
         Receive2 response ->
             ( { model | advanced_response = response }, Cmd.none )
 
